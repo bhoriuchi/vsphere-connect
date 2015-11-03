6a9d52ddbcfd43315dd5f746973d5a1e9b3493d3
Branden Horiuchi <bhoriuchi@gmail.com>
2015-11-03 00:13:09 -0800

    saving

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
  ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
 
 Nodeclipse is free open-source project that grows with your contributions.
+
+
--- example/test.js
+++ example/test.js
@@ -1,9 +1,16 @@
 var connect = require('../lib');
 var cred    = require('../credentials');
 
+var viclient;
 
 connect.client(cred.endpoint, cred.user, cred.password, cred.ignoreSSL)
 .then(function(client) {
-	console.log('ServiceInstance', client);
-	//console.log('session', client.session);
+
+	return client.searchManagedEntities('Folder').then(function(view) {
+		console.log(view);
+		return view;
+	})
+	.then(function() {
+		return client.logOut();
+	});
 });
